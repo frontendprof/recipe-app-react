adbdbebf550623db087fdd9857efff5a2d0a8b2e
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-30 02:11:06 +0500

    Init a new project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const APP_ID="81eb18f8";
+  const APP_KEY="ed1e488ec2179a95964393fea0879a48";
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>Peace</h1>
+    </div>
+  );
+}
+
+export default App;
